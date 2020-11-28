660deed718ff39e0594768a90222cad5941d99ca
hoan-nhan <hmhnhan@gmail.com>
2020-11-28 11:58:22 +0700

    on going

--- src/App.js
+++ src/App.js
@@ -8,35 +8,38 @@ import { Book } from './Components/Book'
 export class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {SearchText:""}
-    this.handleInput=this.handleInput.bind(this)
-    this.getBook=this.getBook.bind(this)
+    this.state = {}
+    this.handleInput = this.handleInput.bind(this)
+    this.getBook = this.getBook.bind(this)
   }
   handleInput(e) {
-    if (e.key==='Enter') {
-      // this.setState ({SearchText: e.target.value})
-      return e.target.value
+    if (e.key === 'Enter') {
+      this.setState({ SearchText: e.target.value })
+      // return e.target.value
       console.log(e.target.value)
     }
   }
-  getBook(searchText) {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchText}`)
-    .then(res=>res.json())
-    // .then(book=>console.log(book))
-    .then(books => this.setState({books}))
-  }
+  // getBook(searchText) {
+  //   fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchText}`)
+  //     .then(res => res.json())
+  //     // .then(book=>console.log(book))
+  //     .then(books => {
+  //       this.setState({ books })
+  //       console.log(books)
+  //     })
+  // }
   genBook() {
     let bookList = [];
     bookList.push(this.state.books.items)
     console.log(bookList)
   }
   render() {
-    console.log(this.state.SearchText)
+    console.log(this.state)
     return (
       <React.Fragment>
-        <Search onClick_={() => this.getBook(this.state.searchText)}/>
-        {/* <Search onClick_={() => this.getBook(this.handleInput)}/> */}
-        <Book/>
+        <Search onKeyDown_={this.getBook(this.handleInput)} />
+
+
       </React.Fragment>
     );
   }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -19,8 +19,8 @@ export function Search(props) {
     return (
         <React.Fragment>
             <h1>Book Finder</h1>
-            <input type='text' placeholder='Type, Author, Book name, Subject...' onClick={props.onClick_}></input>
-            <button type='button'>SEARCH</button>
+            <input type='text' placeholder='Type, Author, Book name, Subject...' onKeyDown={props.onKeyDown_}></input>
+            <button type='button' >SEARCH</button>
         </React.Fragment>
     )
 }
